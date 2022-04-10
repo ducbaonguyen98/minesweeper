5d22746e1672e0ede06062d1df85813a93df49ad
Bão Bão <ducbaonguyen101098@gmail.com>
2022-04-10 18:59:15 +0700

    up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "..", 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
